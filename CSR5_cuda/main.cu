6fd0154295e2676003b0e4bcf90f0df0e9eed2b4
maruf <maruf.csea1@gmail.com>
2019-09-26 17:44:28 -0400

    print gpu info

--- CSR5_cuda/main.cu
+++ CSR5_cuda/main.cu
@@ -156,13 +156,13 @@ int main(int argc, char ** argv)
 {
 
     int rank, size;
-    /*MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Init(&argc, &argv);
+    /*MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 //    cout << "MPI Task " << rank << " of " << size << " starting...."<<endl;
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Finalize();*/
+    MPI_Barrier(MPI_COMM_WORLD);*/
+    MPI_Finalize();
     return 0;
 
     int m, n, nnzA;
