48f9e4819ac46dd68487d0a6ec55681df42cda5d
Md Maruf Hossain <maruf.csea1@gmail.com>
2019-09-26 13:42:04 -0400

    Result sets updated using 1024 partitioning

--- CSR5_cuda/runAll.sh
+++ CSR5_cuda/runAll.sh
@@ -1,5 +1,8 @@
 for filename in input/* 
 do 
-	echo "Run on Matrix: ${filename}"
-	qsub -q copperhead -N "spmv_gpu" -l nodes=$1:ppn=$2:gpus=$3  -v in=${filename} -d $(pwd) run.sh
+	if [ -f "$filename" ] 
+	then 
+		echo "Run on Matrix: ${filename}"
+		qsub -q copperhead -N "spmv_gpu" -l nodes=$1:ppn=$2:gpus=$3  -v in=${filename} -d $(pwd) run.sh
+	fi
 done
