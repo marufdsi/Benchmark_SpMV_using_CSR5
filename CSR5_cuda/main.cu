2e06707c94dd20037d942dcddb7045b42945ecb7
maruf <maruf.csea1@gmail.com>
2019-09-26 17:37:50 -0400

    print gpu info

--- CSR5_cuda/main.cu
+++ CSR5_cuda/main.cu
@@ -163,7 +163,7 @@ int main(int argc, char ** argv)
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    cerr << "MPI Task " << rank << " of " << size << " starting....\n";
+    cout << "MPI Task " << rank << " of " << size << " starting...."<<endl;
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
