2fddf9a48fcebafa946ebea04f1d97f79678f7f5
Md Maruf Hossain <maruf.csea1@gmail.com>
2019-09-28 16:38:08 -0400

    Result sets are added

--- CSR5_cuda/runAll.sh
+++ CSR5_cuda/runAll.sh
@@ -3,6 +3,6 @@ do
 	if [ -f "$filename" ] 
 	then 
 		echo "Run on Matrix: ${filename}"
-		qsub -q copperhead -N "spmv_gpu" -l nodes=$1:ppn=$2:gpus=$3  -v in=${filename} -d $(pwd) run.sh
+		qsub -q copperhead -N "spmv_gpu" -l nodes=$1:ppn=$2:gpus=$3:k80  -v in=${filename},r=$4 -d $(pwd) run.sh
 	fi
 done
